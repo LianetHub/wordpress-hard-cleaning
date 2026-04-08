53b51cb4367701804beaab230633c4056d35d47d
lianet <jack.tolmak@yandex.ru>
2026-04-08 13:30:02 +0300

    add prices page

---
+++ page-prices.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Template Name: Page Prices
+ */
+get_header(); ?>
+
+<?php require_once(TEMPLATE_PATH . '_trust.php'); ?>
+<?php require_once(TEMPLATE_PATH . '_faq.php'); ?>
+<?php require_once(TEMPLATE_PATH . '_cta.php'); ?>
+
+<?php get_footer(); ?>
