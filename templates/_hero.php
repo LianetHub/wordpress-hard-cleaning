b0fbe3bfdd89db6aab1bfc427e926163b4addd66
lianet <jack.tolmak@yandex.ru>
2026-04-08 13:11:31 +0300

    add hero and trust sections to acf data

--- templates/_hero.php
+++ templates/_hero.php
@@ -1,36 +1,47 @@
+ <?php
+    $image = get_field('hero_image');
+    $tags = get_field('hero_tags');
+    $title = get_field('hero_title') ?: 'Комплексная спецуборка в Санкт-Петербурге и области';
+    $subtitle = get_field('hero_subtitle') ?: 'После смерти, пожара, потопа и сильного загрязнения. Оценка в день обращения. Выезд по СПб и Ленобласти.';
+    ?>
+
  <section class="hero">
      <div class="hero__container container">
          <div class="hero__image">
-             <img src="<?php echo get_template_directory_uri(); ?>/assets/hero_cleaners.png"
-                 alt="Спецуборка"
-                 class="hero__image-main"
-                 fetchpriority="high"
-                 loading="eager">
-             <ul class="hero__tags">
-                 <li class="hero__tag">
-                     <div class="hero__tag-icon">
-                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/shield.svg" alt="иконка">
-                     </div>
-                     <div class="hero__tag-text">Дезинфекция</div>
-                 </li>
+             <?php if ($image): ?>
+                 <img src="<?php echo esc_url($image['url']); ?>"
+                     alt="<?php echo esc_attr($image['alt']); ?>"
+                     class="hero__image-main"
+                     fetchpriority="high"
+                     loading="eager">
+             <?php else: ?>
+                 <img src="<?php echo get_template_directory_uri(); ?>/assets/hero_cleaners.png"
+                     alt="Спецуборка"
+                     class="hero__image-main"
+                     fetchpriority="high"
+                     loading="eager">
+             <?php endif; ?>
 
-                 <li class="hero__tag">
-                     <div class="hero__tag-icon">
-                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/ozonation.svg" alt="иконка">
-                     </div>
-                     <div class="hero__tag-text">Озонирование</div>
-                 </li>
-                 <li class="hero__tag">
-                     <div class="hero__tag-icon">
-                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/broom.svg" alt="иконка">
-                     </div>
-                     <div class="hero__tag-text">Просушка помещений</div>
-                 </li>
-             </ul>
+             <?php if ($tags): ?>
+                 <ul class="hero__tags">
+                     <?php foreach ($tags as $tag): ?>
+                         <li class="hero__tag">
+                             <?php if ($tag['icon']): ?>
+                                 <div class="hero__tag-icon">
+                                     <img src="<?php echo esc_url($tag['icon']['url']); ?>"
+                                         alt="<?php echo esc_attr($tag['icon']['alt']); ?>">
+                                 </div>
+                             <?php endif; ?>
+                             <div class="hero__tag-text"><?php echo esc_html($tag['text']); ?></div>
+                         </li>
+                     <?php endforeach; ?>
+                 </ul>
+             <?php endif; ?>
          </div>
+
          <div class="hero__offer">
-             <h1 class="hero__title title-lg">Комплексная спецуборка в&nbsp;Санкт-Петербурге и&nbsp;области</h1>
-             <p class="hero__subtitle">После смерти, пожара, потопа и сильного загрязнения. Оценка в день обращения. Выезд по СПб и Ленобласти.</p>
+             <h1 class="hero__title title-lg"><?php echo $title; ?></h1>
+             <p class="hero__subtitle"><?php echo esc_html($subtitle); ?></p>
              <div class="hero__btns">
                  <a href="#contacts" class="hero__btn btn btn-primary">Срочный вызов</a>
                  <a href="#contacts" class="hero__btn btn btn-outline">Рассчитать стоимость</a>
--- templates/_trust.php
+++ templates/_trust.php
@@ -1,23 +1,44 @@
-<?php if (have_rows('trust_cards')): ?>
-    <div class="trust">
-        <div class="container">
+<?php
+$hint = get_field('trust_hint');
+$title = get_field('trust_title');
+$subtitle = get_field('trust_subtitle');
+$bg_class = get_field('trust_bg_variant') ?: 'bg-white';
+
+$wrapper_tag = $title ? 'section' : 'div';
+?>
+
+<<?php echo $wrapper_tag; ?> class="trust <?php echo esc_attr($bg_class); ?>">
+    <div class="container">
+        <?php if ($hint): ?>
+            <div class="trust__hint hint"><?php echo esc_html($hint); ?></div>
+        <?php endif; ?>
+
+        <?php if ($title): ?>
+            <h2 class="trust__title title"><?php echo $title; ?></h2>
+        <?php endif; ?>
+
+        <?php if ($subtitle): ?>
+            <p class="trust__subtitle"><?php echo esc_html($subtitle); ?></p>
+        <?php endif; ?>
+
+        <?php if (have_rows('trust_cards')): ?>
             <div class="trust__slider swiper">
                 <ul class="swiper-wrapper">
                     <?php while (have_rows('trust_cards')): the_row();
                         $icon = get_sub_field('icon');
-                        $title = get_sub_field('title');
+                        $card_title = get_sub_field('title');
                         $descr = get_sub_field('descr');
                     ?>
                         <li class="trust__card swiper-slide">
                             <?php if ($icon): ?>
                                 <div class="trust__card-icon">
                                     <img src="<?php echo esc_url($icon['url']); ?>"
-                                        alt="<?php echo esc_attr($icon['alt'] ?: $title); ?>">
+                                        alt="<?php echo esc_attr($icon['alt'] ?: $card_title); ?>">
                                 </div>
                             <?php endif; ?>
 
-                            <?php if ($title): ?>
-                                <h4 class="trust__card-caption"><?php echo esc_html($title); ?></h4>
+                            <?php if ($card_title): ?>
+                                <h4 class="trust__card-caption"><?php echo esc_html($card_title); ?></h4>
                             <?php endif; ?>
 
                             <?php if ($descr): ?>
@@ -28,6 +49,6 @@
                 </ul>
                 <div class="trust__slider-pagination swiper-pagination"></div>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
-<?php endif; ?>
+</<?php echo $wrapper_tag; ?>>
