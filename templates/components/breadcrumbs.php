f961717769e85708507baee306ce07bb0d4c4849
lianet <jack.tolmak@yandex.ru>
2026-04-09 14:57:11 +0300

    update breadcrumbs

--- templates/components/breadcrumbs.php
+++ templates/components/breadcrumbs.php
@@ -1,30 +1,54 @@
 <?php
-if (class_exists('WPSEO_Breadcrumbs')) :
-    $breadcrumbs = WPSEO_Breadcrumbs::get_instance()->get_links();
-    if ($breadcrumbs) : ?>
-        <nav aria-label="Хлебные крошки" class="breadcrumbs">
-            <div class="container">
-                <ul class="breadcrumbs__list">
-                    <?php foreach ($breadcrumbs as $i => $breadcrumb) :
-                        $text = isset($breadcrumb['text']) ? esc_html($breadcrumb['text']) : '';
-                        $url = isset($breadcrumb['url']) ? esc_url($breadcrumb['url']) : '';
+$queried_object = get_queried_object();
+$items = [];
 
-                        if (empty($text)) continue;
-                    ?>
-                        <li class="breadcrumbs__item">
-                            <?php if ($i < count($breadcrumbs) - 1) : ?>
-                                <a href="<?php echo $url; ?>" class="breadcrumbs__link">
-                                    <?php echo $text; ?>
-                                </a>
-                            <?php else : ?>
-                                <span class="breadcrumbs__current" aria-current="page">
-                                    <?php echo $text; ?>
-                                </span>
-                            <?php endif; ?>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        </nav>
-<?php endif;
-endif; ?>
+$items[] = ['name' => 'Главная', 'link' => home_url('/')];
+
+
+$items[] = ['name' => 'Услуги', 'link' => get_post_type_archive_link('services')];
+
+if (is_tax('service_cat')) {
+    $ancestors = get_ancestors($queried_object->term_id, 'service_cat');
+    foreach (array_reverse($ancestors) as $ancestor_id) {
+        $ancestor = get_term($ancestor_id);
+        $items[] = ['name' => $ancestor->name, 'link' => get_term_link($ancestor)];
+    }
+    $items[] = ['name' => $queried_object->name, 'link' => ''];
+} elseif (is_singular('services')) {
+
+    $terms = get_the_terms(get_the_ID(), 'service_cat');
+    if ($terms) {
+        $main_term = $terms[0];
+        foreach ($terms as $term) {
+            if ($term->parent != 0) {
+                $main_term = $term;
+                break;
+            }
+        }
+
+        $ancestors = get_ancestors($main_term->term_id, 'service_cat');
+        foreach (array_reverse($ancestors) as $ancestor_id) {
+            $ancestor = get_term($ancestor_id);
+            $items[] = ['name' => $ancestor->name, 'link' => get_term_link($ancestor)];
+        }
+        $items[] = ['name' => $main_term->name, 'link' => get_term_link($main_term)];
+    }
+    $items[] = ['name' => get_the_title(), 'link' => ''];
+}
+?>
+
+<nav aria-label="Хлебные крошки" class="breadcrumbs">
+    <div class="container">
+        <ul class="breadcrumbs__list">
+            <?php foreach ($items as $index => $item) : ?>
+                <li class="breadcrumbs__item">
+                    <?php if ($item['link'] && $index < count($items) - 1) : ?>
+                        <a href="<?php echo esc_url($item['link']); ?>"><?php echo esc_html($item['name']); ?></a>
+                    <?php else : ?>
+                        <span><?php echo esc_html($item['name']); ?></span>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</nav>
