cff36a2986ca0718b54e899ddf90c1ebb3e987bd
lianet <jack.tolmak@yandex.ru>
2026-05-03 21:35:08 +0300

    fix small bugs in works

--- templates/_works-specialized.php
+++ templates/_works-specialized.php
@@ -1,66 +1,35 @@
 <?php
+$specialized_cases = [215, 242, 224, 950, 1027, 1050];
 
-$specialized_cat_ids = [
-    6,
-    4,
-    8,
-    10,
-    5,
-    12,
-    28
-];
-
-$specialized_posts = [];
-
-
-foreach ($specialized_cat_ids as $cat_id) {
+if (!empty($specialized_cases)) :
     $args = [
         'post_type'      => 'portfolio',
-        'posts_per_page' => 1,
-        'fields'         => 'ids',
-        'meta_query'     => [
-            [
-                'key'     => 'case_service_link',
-                'value'   => '"' . $cat_id . '"',
-                'compare' => 'LIKE',
-            ],
-        ],
+        'post__in'       => $specialized_cases,
+        'orderby'        => 'post__in',
+        'posts_per_page' => -1,
     ];
 
-    $query = new WP_Query($args);
+    $works_query = new WP_Query($args);
 
-    if (!empty($query->posts)) {
-        $specialized_posts[] = $query->posts[0];
-    }
-}
+    if ($works_query->have_posts()) : ?>
+        <section class="works works--specialized">
+            <div class="container">
+                <div class="works__hint hint">Примеры выполненных работ</div>
+                <h2 class="works__title title">Результаты нашей <span class="color-accent">спецуборки</span></h2>
 
+                <div class="works__grid">
+                    <?php while ($works_query->have_posts()) : $works_query->the_post(); ?>
+                        <?php get_template_part('templates/components/card-portfolio'); ?>
+                    <?php endwhile; ?>
+                </div>
 
-if (!empty($specialized_posts)) : ?>
-    <section class="works works--specialized">
-        <div class="container">
-            <div class="works__hint hint">Примеры выполненных работ</div>
-            <h2 class="works__title title">Результаты нашей <span class="color-accent">спецуборки</span></h2>
-
-            <div class="works__grid">
-                <?php
-                $final_query = new WP_Query([
-                    'post_type' => 'portfolio',
-                    'post__in'  => $specialized_posts,
-                    'orderby'   => 'post__in'
-                ]);
-
-                if ($final_query->have_posts()) :
-                    while ($final_query->have_posts()) : $final_query->the_post();
-                        get_template_part('templates/components/card-portfolio');
-                    endwhile;
-                endif;
-                wp_reset_postdata();
-                ?>
+                <a href="<?php echo get_post_type_archive_link('portfolio') ?>" class="cases__more btn btn-primary">
+                    Посмотреть все работы →
+                </a>
             </div>
-
-            <a href="<?php echo get_post_type_archive_link('portfolio') ?>" class="cases__more btn btn-primary">
-                Посмотреть все кейсы →
-            </a>
-        </div>
-    </section>
-<?php endif; ?>
+        </section>
+<?php
+    endif;
+    wp_reset_postdata();
+endif;
+?>
